62cd67a71050bb6c55093927d3568152d608c09f
Chris Tan <csytan@gmail.com>
2014-01-29 16:44:59 -0500

    Basic project setup
    - Setting up templating
    - Email reply filtering
    - Thinking about how models will be organized

--- main.py
+++ main.py
@@ -17,29 +17,70 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 
+### Models ###
+
+class User(ndb.Model):
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    github_id = ndb.StringProperty()
+
+    @classmethod
+    def create(cls, name, **kwargs):
+        id = name.lower().strip().replace(' ', '_')
+        user = cls(id=id)
+        user.populate(**kwargs)
+        user.put()
+        return user
+
+
 class Update(ndb.Model):
     created = ndb.DateTimeProperty(auto_now_add=True)
     text = ndb.TextProperty()
 
 
-class MainHandler(webapp2.RequestHandler):
+
+### Handlers ###
+
+class BaseHandler(webapp2.RequestHandler):
+    def write_template(self, file_name, context={}):
+        template = JINJA_ENVIRONMENT.get_template(file_name)
+        output = template.render(context)
+        self.response.write(output)
+
+
+class IndexHandler(BaseHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.write_template('index.html')
+        
 
 
-class EmailHandler(webapp2.RequestHandler):
+class PersonHandler(BaseHandler):
+    def get(self):
+        self.write_template('index.html')
+
+
+class EmailHandler(BaseHandler):
     def post(self):
         message = mail.InboundEmailMessage(self.request.body)
         logging.debug(message.sender)
         logging.debug(message.subject)
-        for body in message.bodies('text/plain'):
-            logging.debug(str(body[1].decode()))
+
+        for msg_type, text in message.bodies('text/plain'):
+            body = ''
+            for line in text.decode().splitlines():
+                if line.startswith('On') and line.endswith('wrote:'):
+                    break
+                else:
+                    body += line + '\n'
+            body = body.strip()
+            logging.debug(body)
 
 
 
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
+    ('/', IndexHandler),
+    ('/(.+@.+)', PersonHandler),
     ('/_ah/mail/.+', EmailHandler)
 ], debug=True)
